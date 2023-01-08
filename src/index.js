03bbac8a243d560cb538435e8beb6fc0268820d2
yesnein <yesnein@googlemail.com>
2023-01-08 19:48:32 +0100

    [API] Vercel boilerplate

--- src/index.js
+++ src/index.js
@@ -9,19 +9,12 @@ import { BasicStrategy } from 'passport-http'
 
 const app = express()
 
-const isProd = (process.env.ENV === 'production')
-const key = isProd ? fs.readFileSync('/etc/letsencrypt/live/h2939755.stratoserver.net/privkey.pem', 'utf8') : ''
-const cert = isProd ? fs.readFileSync('/etc/letsencrypt/live/h2939755.stratoserver.net/cert.pem', 'utf8') : ''
-const ca = isProd ? fs.readFileSync('/etc/letsencrypt/live/h2939755.stratoserver.net/chain.pem', 'utf8') : ''
-
-const credentials = { key, cert, ca }
-
 const users = {
     user: 'password'
 }
 
 passport.use(new BasicStrategy((username, password, done) => {
-    if (!users[username] || users[username] != password) {
+    if (!users[username] || users[username] !== password) {
         return done (null, false)
     }
     return done(null, username)
@@ -63,15 +56,8 @@ app.post('/messages',
     return res.send(message)
 })
 
-const httpServer = http.createServer(app)
-
-httpServer.listen(process.env.HTTP_PORT, () => {
+app.listen(process.env.HTTP_PORT, () => {
     console.log(`Express HTTP serving running port ${process.env.HTTP_PORT}!`)
 })
 
-if (isProd) {
-    const httpsServer = https.createServer(credentials, app)
-    httpsServer.listen(process.env.HTTPS_PORT, () => {
-        console.log(`Express HTTP serving running port ${process.env.HTTPS_PORT}!`)
-    })
-}
+export default app
